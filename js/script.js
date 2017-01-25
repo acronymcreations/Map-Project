2d17cfcecc9ca5f519256ba7d7162c04d9ff16bb
Gabel Wright <acronymcreations@gmail.com>
2017-01-25 12:52:45 -0700

    Map is updated with new pins when pins() changes
    * Whenever the pins() array changes, the map is updated to show the new set
    of pins.
    * Layout of page was adjusted so the results list and map fit on the page
    better.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ var pins = ko.observableArray();
 
 var placeName = ko.observable("Bert");
 
-
-
 function pin(name,lat,long){
     var self = this;
     self.name = name;
@@ -14,18 +12,36 @@ function pin(name,lat,long){
 
 function initMap() {
     console.log('method ran');
-    var uluru = {lat: 34.869445, lng: -111.761493};
+    var sedonaLoaction = {lat: 34.869445, lng: -111.761493};
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 18,
-        center: uluru
+        zoom: 14,
+        center: sedonaLoaction
     });
-    for(var i=0;i<pins.length;i++){
+    var marker = new google.maps.Marker({
+        position: sedonaLoaction,
+        map: map
+    });
+    console.log("start of forloop");
+    console.log(pins().length);
+    for(var i=0;i<pins().length;i++){
+        var lati = pins()[i].lat;
+        var longi = pins()[i].long;
+        var pinLocation = {lat: lati, lng: longi};
         var marker = new google.maps.Marker({
-            position: pins[i].location,
-            map: map
+            position: pinLocation,
+            map: map,
+            title: pins()[i].name,
+            optimized: false,
+            animation: google.maps.Animation.DROP
         });
+        marker.addListener('click',pinClicked);
+        
     }
     
+}
+
+function pinClicked(pin){
+    console.log('pointer was clicked');
 }
 
 function yelpCallBack(data){
@@ -34,65 +50,70 @@ function yelpCallBack(data){
         var name = data.businesses[i].name;
         var lat = data.businesses[i].location.coordinate.latitude;
         var long = data.businesses[i].location.coordinate.longitude;
-        console.log(new pin(name,lat,long));
         pins.push(new pin(name,lat,long));
     }
     console.log('here are the pins');
     console.log(pins());
+    initMap();
     
 }
 
-var auth = {
-    consumerKey: 'MP8vIWLoeNB-EpuQyz1sZw',
-    consumerSecret: 'hHAFwe1_PPdTBjj9xfUNVcIuboQ',
-    accessToken: '8e8nvvdSRuRhabjKWos6_R-lDq0vDc83',
-    accessTokenSecret: 'R9do-WPnVs0Y3_SDjKW4EBDR7lc',
-    serviceProvider: {
-        signatureMethod: 'HMAC-SHA1'
-    }
-};
+function initYelp(searchTerm){
+    var auth = {
+        consumerKey: 'MP8vIWLoeNB-EpuQyz1sZw',
+        consumerSecret: 'hHAFwe1_PPdTBjj9xfUNVcIuboQ',
+        accessToken: '8e8nvvdSRuRhabjKWos6_R-lDq0vDc83',
+        accessTokenSecret: 'R9do-WPnVs0Y3_SDjKW4EBDR7lc',
+        serviceProvider: {
+            signatureMethod: 'HMAC-SHA1'
+        }
+    };
 
-var searchTerm = 'food'
-var near = 'sedona'
+    var searchTerm = searchTerm;
+    var near = 'sedona';
 
-var accessor = {
-    consumerSecret: auth.consumerSecret,
-    tokenSecret: auth.accessTokenSecret
-};
+    var accessor = {
+        consumerSecret: auth.consumerSecret,
+        tokenSecret: auth.accessTokenSecret
+    };
 
-var parameters = [];
-parameters.push(['term',searchTerm]);
-parameters.push(['location', near]);
-parameters.push(['callback', 'yelpCallBack']);
-parameters.push(['oauth_consumer_key', auth.consumerKey]);
-parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
-parameters.push(['oauth_token', auth.accessToken]);
-parameters.push(['oauth_signature_method', 'HMAC-SHA1']); 
+    var parameters = [];
+    parameters.push(['term',searchTerm]);
+    parameters.push(['location', near]);
+    parameters.push(['callback', 'yelpCallBack']);
+    parameters.push(['oauth_consumer_key', auth.consumerKey]);
+    parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+    parameters.push(['oauth_token', auth.accessToken]);
+    parameters.push(['oauth_signature_method', 'HMAC-SHA1']); 
 
-var message = {
-    'action' : 'https://api.yelp.com/v2/search',
-    'method' : 'GET',
-    'parameters' : parameters
-};     
+    var message = {
+        'action' : 'https://api.yelp.com/v2/search',
+        'method' : 'GET',
+        'parameters' : parameters
+    };     
 
-OAuth.setTimestampAndNonce(message);
-OAuth.SignatureMethod.sign(message, accessor);
+    OAuth.setTimestampAndNonce(message);
+    OAuth.SignatureMethod.sign(message, accessor);
 
-var parameterMap = OAuth.getParameterMap(message.parameters);
-    
-$.ajax({
-    'url' : message.action,
-    'data' : parameterMap,
-    'dataType' : 'jsonp',
-    'jsonpCallback' : 'yelpCallBack',
-    'cache': true
-})
-.done(function(data, textStatus, jqXHR) {
-    console.log('done');
-})
-.fail(function(jqXHR, textStatus, errorThrown) {
-    console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
-});
+    var parameterMap = OAuth.getParameterMap(message.parameters);
+        
+    $.ajax({
+        'url' : message.action,
+        'data' : parameterMap,
+        'dataType' : 'jsonp',
+        'jsonpCallback' : 'yelpCallBack',
+        'cache': true
+    })
+    .done(function(data, textStatus, jqXHR) {
+        console.log('done');
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('error[' + errorThrown + '], status[' + textStatus + '], jqXHR[' + JSON.stringify(jqXHR) + ']');
+    });
+}
+
+initYelp('coffee');
+
 
 
 ko.applyBindings();
